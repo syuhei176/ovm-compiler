8ce88a96f04ece8d7acb81ac65fdc6d0262e45fc
SHUHEI HIYA <syuhei176@gmail.com>
2019-12-14 01:11:43 +0900

    fix: generator bug

--- examples/checkpoint/Checkpoint.sol
+++ examples/checkpoint/Checkpoint.sol
@@ -7,21 +7,16 @@ import "./AtomicPredicate.sol";
 import "./NotPredicate.sol";
 
 
-
 /**
  * Checkpoint(B,C)
  */
 contract Checkpoint {
-  
-    bytes32 public CheckpointFFO2A;
-  
-    bytes32 public CheckpointFFO;
-  
-    bytes32 public CheckpointFF;
-  
-    bytes32 public CheckpointF;
-  
-    UniversalAdjudicationContract AdjudicationContract;
+    bytes public CheckpointFFO2A = bytes("CheckpointFFO2A");
+    bytes public CheckpointFFO = bytes("CheckpointFFO");
+    bytes public CheckpointFF = bytes("CheckpointFF");
+    bytes public CheckpointF = bytes("CheckpointF");
+
+    UniversalAdjudicationContract adjudicationContract;
     AtomicPredicate SU;
     AtomicPredicate LessThan;
     AtomicPredicate eval;
@@ -29,74 +24,63 @@ contract Checkpoint {
     AtomicPredicate SameRange;
     AtomicPredicate IsValidSignature;
     NotPredicate Not;
+
     constructor(address _adjudicationContractAddress) {
-      AdjudicationContract = UniversalAdjudicationContract(_adjudicationContractAddress);
-      
-      CheckpointFFO2A = keccak256("CheckpointFFO2A");
-      
-      CheckpointFFO = keccak256("CheckpointFFO");
-      
-      CheckpointFF = keccak256("CheckpointFF");
-      
-      CheckpointF = keccak256("CheckpointF");
-      
+        adjudicationContract = UniversalAdjudicationContract(_adjudicationContractAddress);
     }
+
     /**
-    * @dev Validates a child node of the property in game tree.
-    */
+     * @dev Validates a child node of the property in game tree.
+     */
     function isValidChallenge(
         bytes[] memory _inputs,
-        bytes memory _challengeInput,
+        bytes[] memory _challengeInput,
         types.Property memory _challenge
     ) public returns (bool) {
         require(
-          keccak256(abi.encode(getChild(_inputs, _challengeInput))) == keccak256(abi.encode(_challenge)),
-          "_challenge must be valud child of game tree"
+            keccak256(abi.encode(getChild(_inputs, _challengeInput))) == keccak256(abi.encode(_challenge)),
+            "_challenge must be valud child of game tree"
         );
         return true;
     }
 
-    function getChild(bytes[] memory inputs, bytes memory challengeInput) private returns (types.Property memory) {
-      bytes32 input0 = bytesToBytes32(inputs[0]);
-      
-        
+    function getChild(
+        bytes[] memory inputs,
+        bytes[] memory challengeInput
+    ) private returns (types.Property memory) {
+        bytes32 input0 = bytesToBytes32(inputs[0]);
         if(input0 == CheckpointFFO2A) {
-          return getChildCheckpointFFO2A(inputs, challengeInput);
+            return getChildCheckpointFFO2A(inputs, challengeInput);
         }
-        
-        
+        if(input0 == CheckpointFFO) {
+            return getChildCheckpointFFO(inputs, challengeInput);
+        }
         if(input0 == CheckpointFF) {
-          return getChildCheckpointFF(inputs, challengeInput);
+            return getChildCheckpointFF(inputs, challengeInput);
         }
-        
         if(input0 == CheckpointF) {
-          return getChildCheckpointF(inputs, challengeInput);
+            return getChildCheckpointF(inputs, challengeInput);
         }
     }
 
-  /**
-   * @dev check the property is true
-   */
-  function decide(bytes[] memory _inputs, bytes memory _witness) public view returns(bool) {
-    bytes32 input0 = bytesToBytes32(_inputs[0]);
-    
-    if(input0 == CheckpointFFO2A) {
-      decideCheckpointFFO2A(_inputs, _witness);
+    /**
+     * @dev check the property is true
+     */
+    function decide(bytes[] memory _inputs, bytes memory _witness) public view returns(bool) {
+        bytes32 input0 = bytesToBytes32(_inputs[0]);
+        if(input0 == CheckpointFFO2A) {
+            decideCheckpointFFO2A(_inputs, _witness);
+        }
+        if(input0 == CheckpointFFO) {
+            decideCheckpointFFO(_inputs, _witness);
+        }
+        if(input0 == CheckpointFF) {
+            decideCheckpointFF(_inputs, _witness);
+        }
+        if(input0 == CheckpointF) {
+            decideCheckpointF(_inputs, _witness);
+        }
     }
-    
-    if(input0 == CheckpointFFO) {
-      decideCheckpointFFO(_inputs, _witness);
-    }
-    
-    if(input0 == CheckpointFF) {
-      decideCheckpointFF(_inputs, _witness);
-    }
-    
-    if(input0 == CheckpointF) {
-      decideCheckpointF(_inputs, _witness);
-    }
-    
-  }
 
     function decideTrue(bytes[] memory _inputs, bytes[] memory _witness) public {
         require(decide(_inputs, _witness), "must be true");
@@ -107,99 +91,165 @@ contract Checkpoint {
         adjudicationContract.setPredicateDecision(utils.getPropertyId(property), true);
     }
 
-  
-  
     /**
      * Gets child of CheckpointFFO2A().
      */
-    function getChildCheckpointFFO2A(bytes[] memory _inputs, bytes memory challengeInput) private returns (types.Property memory) {
-      
-        
+    function getChildCheckpointFFO2A(bytes[] memory _inputs, bytes[] memory challengeInputs) private returns (types.Property memory) {
+        uint256 challengeInput = abi.decode(challengeInputs[0], (uint256));
+        bytes[] memory notInputs = new bytes[](1);
         if(challengeInput == 0) {
-      
-          return type.Property({
-            predicateAddress: Not,
-            inputs: [_inputs[1]]
-          });
-      
+            types.Property memory inputPredicateProperty = abi.decode(_inputs[1], (types.Property));
+            bytes[] memory childInputs = new bytes[](inputPredicateProperty.inputs.length + 1);
+            for(uint256 i = 0;i < inputPredicateProperty.inputs.length;i++) {
+                childInputs[i] = inputPredicateProperty.inputs[i];
+            }
+            childInputs[stateObject.inputs.length] = _inputs[2];
+            notInputs[0] = abi.encode(types.Property({
+                predicateAddress: inputPredicateProperty.predicateAddress,
+                inputs: childInputs
+            }));
         }
-        
         if(challengeInput == 1) {
-      
-          return type.Property({
-            predicateAddress: Not,
-            inputs: [_inputs[3]]
-          });
-      
+            types.Property memory inputPredicateProperty = abi.decode(_inputs[3], (types.Property));
+            bytes[] memory childInputs = new bytes[](inputPredicateProperty.inputs.length + 1);
+            for(uint256 i = 0;i < inputPredicateProperty.inputs.length;i++) {
+                childInputs[i] = inputPredicateProperty.inputs[i];
+            }
+            childInputs[stateObject.inputs.length] = _inputs[2];
+            notInputs[0] = abi.encode(types.Property({
+                predicateAddress: inputPredicateProperty.predicateAddress,
+                inputs: childInputs
+            }));
         }
-        
-      
+        return type.Property({
+            predicateAddress: notAddress,
+            inputs: notInputs
+        });
     }
-  
-  /**
-   * Decides CheckpointFFO2A(CheckpointFFO2A,verifyInclusion,su,eq,ZeroHash).
-   */
-  function decideCheckpointFFO2A(bytes[] memory _inputs, bytes[] memory _witness) public view returns (bool) {
-    
-      // And logical connective
-      
-      require([object Object].decide([_inputs[2]], _witness));
-      
-      require([object Object].decide([_inputs[2],_inputs[4]], _witness));
-      
-    
-  }
-  
-  
-  /**
-   * Decides CheckpointFFO(CheckpointFFO,su,verifyInclusion,eq,ZeroHash).
-   */
-  function decideCheckpointFFO(bytes[] memory _inputs, bytes[] memory _witness) public view returns (bool) {
-    
-      // check Or
-    var result = false
-      
-    result = result | require([object Object].decide([], _witness))
-      
-    result = result | require(CheckpointFFO2A.decide([challengeInput,_inputs[2],_inputs[1],_inputs[3],_inputs[4]], _witness))
-      
-    require(result);
-    
-  }
-  
-  
+    /**
+     * Gets child of CheckpointFFO().
+     */
+    function getChildCheckpointFFO(bytes[] memory _inputs, bytes[] memory challengeInputs) private returns (types.Property memory) {
+
+        bytes[] memory andInputs = new bytes[](2);
+        bytes[] memory notInputs0 = new bytes[](1);
+            notInputs0[0] = _inputs[1];
+        andInputs[0] = abi.encode(type.Property({
+            predicateAddress: notAddress,
+            inputs: notInputs0
+        }));
+        bytes[] memory notInputs1 = new bytes[](1);
+            bytes[] memory childInputs = new bytes[](2);
+            childInputs[0] = CheckpointFFO2A;
+            childInputs[1] = _inputs[2];
+            childInputs[2] = _inputs[1];
+            childInputs[3] = _inputs[3];
+            childInputs[4] = _inputs[4];
+            notInputs1[0] = abi.encode(type.Property({
+                predicateAddress: CheckpointFFO2A,
+                inputs: childInputs
+            }));
+        andInputs[1] = abi.encode(type.Property({
+            predicateAddress: notAddress,
+            inputs: notInputs1
+        }));
+        return type.Property({
+            predicateAddress: And,
+            inputs: andInputs
+        });
+    }
     /**
      * Gets child of CheckpointFF().
      */
-    function getChildCheckpointFF(bytes[] memory _inputs, bytes memory challengeInput) private returns (types.Property memory) {
-        require(SU.decide(_inputs[1],_inputs[2], challengeInput));
-        return getChildCheckpointFFO([challengeInput,challengeInput,_inputs[3],_inputs[4],_inputs[5]], challengeInputs.subArray(1));
-            
+    function getChildCheckpointFF(bytes[] memory _inputs, bytes[] memory challengeInputs) private returns (types.Property memory) {
+        bytes[] memory quantifierInputs = new bytes[](3);
+            quantifierInputs[0] = _inputs[1];
+            quantifierInputs[1] = _inputs[2];
+        quantifierInputs[2] = challengeInputs[0];
+        require(AtomicPredicate(SU).decide(quantifierInputs));
+        bytes[] memory childInputs = new bytes[](5);
+            childInputs[0] = CheckpointFFO;
+            childInputs[1] = challengeInputs[0];
+            childInputs[2] = _inputs[3];
+            childInputs[3] = _inputs[4];
+            childInputs[4] = _inputs[5];
+        return getChildCheckpointFFO(childInputs, Utils.subArray(challengeInputs, 1, challengeInputs.length));
     }
-  
-  /**
-   * Decides CheckpointFF(CheckpointFF,B,C,verifyInclusion,eq,ZeroHash).
-   */
-  function decideCheckpointFF(bytes[] memory _inputs, bytes[] memory _witness) public view returns (bool) {
-    
-  }
-  
-  
     /**
      * Gets child of CheckpointF().
      */
-    function getChildCheckpointF(bytes[] memory _inputs, bytes memory challengeInput) private returns (types.Property memory) {
-        require(LessThan.decide(_inputs[1], challengeInput));
-        return getChildCheckpointFF([challengeInput,_inputs[1],_inputs[2],_inputs[3],_inputs[4],_inputs[5]], challengeInputs.subArray(1));
-            
+    function getChildCheckpointF(bytes[] memory _inputs, bytes[] memory challengeInputs) private returns (types.Property memory) {
+        bytes[] memory quantifierInputs = new bytes[](2);
+            quantifierInputs[0] = _inputs[1];
+        quantifierInputs[1] = challengeInputs[0];
+        require(AtomicPredicate(LessThan).decide(quantifierInputs));
+        bytes[] memory childInputs = new bytes[](6);
+            childInputs[0] = CheckpointFF;
+            childInputs[1] = _inputs[1];
+            childInputs[2] = _inputs[2];
+            childInputs[3] = _inputs[3];
+            childInputs[4] = _inputs[4];
+            childInputs[5] = _inputs[5];
+        return getChildCheckpointFF(childInputs, Utils.subArray(challengeInputs, 1, challengeInputs.length));
     }
-  
-  /**
-   * Decides CheckpointF(CheckpointF,B,C,verifyInclusion,eq,ZeroHash).
-   */
-  function decideCheckpointF(bytes[] memory _inputs, bytes[] memory _witness) public view returns (bool) {
-    
-  }
-  
+    /**
+     * Decides CheckpointFFO2A(CheckpointFFO2A,verifyInclusion,su,eq,ZeroHash).
+     */
+    function decideCheckpointFFO2A(bytes[] memory _inputs, bytes[] memory _witness) public view returns (bool) {
+        // And logical connective
+
+            types.Property memory inputPredicateProperty = abi.decode(_inputs[1], (types.Property));
+            bytes[] memory childInputs0 = new bytes[](inputPredicateProperty.inputs.length + 1);
+            for(uint256 i = 0;i < inputPredicateProperty.inputs.length;i++) {
+                childInputs0[i] = inputPredicateProperty.inputs[i];
+            }
+            childInputs0[stateObject.inputs.length] = _inputs[2];
+            require(CompiledPredicate(inputPredicateProperty.predicateAddress).decide(childInputs0, Utils.subArray(_witness, 1, _witness.length)));
+
+
+            types.Property memory inputPredicateProperty = abi.decode(_inputs[3], (types.Property));
+            bytes[] memory childInputs1 = new bytes[](inputPredicateProperty.inputs.length + 1);
+            for(uint256 i = 0;i < inputPredicateProperty.inputs.length;i++) {
+                childInputs1[i] = inputPredicateProperty.inputs[i];
+            }
+            childInputs1[stateObject.inputs.length] = _inputs[2];
+            require(CompiledPredicate(inputPredicateProperty.predicateAddress).decide(childInputs1, Utils.subArray(_witness, 1, _witness.length)));
+
+        return true;
+    }
+    /**
+     * Decides CheckpointFFO(CheckpointFFO,su,verifyInclusion,eq,ZeroHash).
+     */
+    function decideCheckpointFFO(bytes[] memory _inputs, bytes[] memory _witness) public view returns (bool) {
+        // check Or
+        var result = false
+        bytes[] memory childInputs0 = new bytes[](0);
+
+        result = result | [object Object].decide(childInputs);
+
+        bytes[] memory childInputs1 = new bytes[](5);
+            childInputs1[0] = CheckpointFFO2A;
+            childInputs1[1] = _inputs[2];
+            childInputs1[2] = _inputs[1];
+            childInputs1[3] = _inputs[3];
+            childInputs1[4] = _inputs[4];
+        result = result | decideCheckpointFFO2A(childInputs, Utils.subArray(_witness, 1, _witness.length));
+
+        require(result);
+        return true;
+    }
+    /**
+     * Decides CheckpointFF(CheckpointFF,B,C,verifyInclusion,eq,ZeroHash).
+     */
+    function decideCheckpointFF(bytes[] memory _inputs, bytes[] memory _witness) public view returns (bool) {
+        return false;
+    }
+    /**
+     * Decides CheckpointF(CheckpointF,B,C,verifyInclusion,eq,ZeroHash).
+     */
+    function decideCheckpointF(bytes[] memory _inputs, bytes[] memory _witness) public view returns (bool) {
+        return false;
+    }
+
 }
 
